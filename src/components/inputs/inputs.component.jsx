ee6801b7f0de3c2f315606a2cade43453349c367
Csaba Boros <boros.csaba94@gmail.com>
2022-04-01 18:27:08 +0300

    Inputs layout

--- src/components/inputs/inputs.component.jsx
+++ src/components/inputs/inputs.component.jsx
@@ -1,22 +1,64 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
+import './inputs.component.scss';
 
 function Inputs() {
 
     return (
         <div class="inputs-container">
-            <FloatingLabel label="Starting amount">
-                <Form.Control type="number" />
-            </FloatingLabel>
-            <FloatingLabel label="Additional contribution">
-                <Form.Control type="number" />
-            </FloatingLabel>
-            <FloatingLabel label="Rate of return">
-                <Form.Control type="number" />
-            </FloatingLabel>
-            <FloatingLabel label="Years to grow">
-                <Form.Control type="number" />
-            </FloatingLabel>
+            <Form>
+                <Row className="mb-3">
+                    <Form.Group>
+                        <Form.Label>Starting amount</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text>$</InputGroup.Text>
+                            <Form.Control type="number"></Form.Control>
+                        </InputGroup>
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Additional contribution</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text>$</InputGroup.Text>
+                            <Form.Control type="number"></Form.Control>
+                        </InputGroup>
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Label>Frequency</Form.Label>
+                        <Form.Select default="Monthly">
+                            <option>Weekly</option>
+                            <option>Bi-weekly</option>
+                            <option>Monthly</option>
+                            <option>Semi-anually</option>
+                            <option>Anually</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Rate of return</Form.Label>
+                        <InputGroup>
+                            <Form.Control type="numer"></Form.Control>
+                            <InputGroup.Text>%</InputGroup.Text>
+                        </InputGroup>
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Label>Years</Form.Label>
+                        <InputGroup>
+                            <Form.Control type="numer"></Form.Control>
+                            <InputGroup.Text>yrs.</InputGroup.Text>
+                        </InputGroup>
+                    </Form.Group>
+                </Row>
+            </Form>
         </div>
     )
 }
