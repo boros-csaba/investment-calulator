3f4ca6a8ddfdfcb58066409230fec03ac1a077ea
Csaba Boros <boros.csaba94@gmail.com>
2022-04-03 22:18:22 +0200

    Breakdown with yearly contributions

--- src/components/yearly-breakdown/yearly-breakdown.component.jsx
+++ src/components/yearly-breakdown/yearly-breakdown.component.jsx
@@ -22,14 +22,14 @@ function YearlyBreakdown() {
             <tbody>
                 {calculateTableData(inputs).map(data => {
                     return (
-                        <tr>
-                            <td>{data.year}</td>
-                            <td>${data.startAmount.toLocaleString()}</td>
-                            <td>${data.additionalContribution.toLocaleString()}</td>
-                            <td>${data.totalContribution.toLocaleString()}</td>
-                            <td>Interest earned</td>
-                            <td>Total interest earned</td>
-                            <td>End balance</td>
+                        <tr key={data.year}>
+                            <td>Year {data.year}</td>
+                            <td>${(+data.startAmount.toFixed(0)).toLocaleString()}</td>
+                            <td>${(+data.additionalContribution.toFixed(0)).toLocaleString()}</td>
+                            <td>${(+data.totalContribution.toFixed(0)).toLocaleString()}</td>
+                            <td>${(+data.interestEarned.toFixed(0)).toLocaleString()}</td>
+                            <td>${(+data.totalInterestEarned.toFixed(0)).toLocaleString()}</td>
+                            <td>${(+data.endBalance.toFixed(0)).toLocaleString()}</td>
                         </tr>
                     )
                 })}
@@ -39,28 +39,40 @@ function YearlyBreakdown() {
 }
 
 const calculateTableData = (inputs) => {
-    const { years, startAmount, additionalContribution, frequency } = inputs;
-    const startYear = new Date().getFullYear();
+    const { years, startAmount, additionalContribution, frequency, rateOfReturn } = inputs;
     const result = [];
 
-    console.log(frequency);
-
     let totalContribution = 0;
+    let endBalance = +startAmount;
     for (var i = 0; i < years; i++) {
-        const year = startYear + i;
+        const year = 1 + i;
 
-        let contribution = +additionalContribution;;
-        if (+frequency === 1) contribution = +additionalContribution * 52
-        if (+frequency === 2) contribution = +additionalContribution * 26;
-        if (+frequency === 3) contribution = +additionalContribution * 12;
-        if (+frequency === 4) contribution = +additionalContribution * 2;
+        let contribution = +additionalContribution;
+        let interestEarned = endBalance * rateOfReturn/100;
+
+        if (+frequency === 1) {
+            contribution = +additionalContribution * 52
+        }
+        if (+frequency === 2) {
+            contribution = +additionalContribution * 26;
+        }
+        if (+frequency === 3) {
+            contribution = +additionalContribution * 12;
+        }
+        if (+frequency === 4) {
+            contribution = +additionalContribution * 2;
+        }
 
         totalContribution += contribution;
+        endBalance += contribution + interestEarned;
         result.push({
             year: year,
             startAmount: +startAmount,
             additionalContribution: +contribution,
-            totalContribution: +totalContribution
+            totalContribution: +totalContribution,
+            interestEarned: interestEarned,
+            totalInterestEarned: interestEarned,
+            endBalance: endBalance
         });
     }
 
--- src/contexts/inputs.context.jsx
+++ src/contexts/inputs.context.jsx
@@ -7,11 +7,11 @@ export const InputsContext = createContext({
 
 export const InputsProvider = ({ children }) => {
     const [inputs, setInputs] = useState({ 
-        startAmount: '10000',
-        additionalContribution: '1000',
-        rateOfReturn: '6.0',
-        years: '10',
-        frequency: '3'
+        startAmount: 10000,
+        additionalContribution: 1000,
+        rateOfReturn: 6.0,
+        years: 10,
+        frequency: 3
     });
     const value = { inputs, setInputs };
     return <InputsContext.Provider value={value}>{children}</InputsContext.Provider>
