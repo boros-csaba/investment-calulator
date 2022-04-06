f57ecad36d31777384f17fe2df9f990b9459a67c
Csaba Boros <boros.csaba94@gmail.com>
2022-04-06 23:47:06 +0200

    Bar chart with dummy data

--- src/components/bar-chart/bar-chart.component.jsx
+++ src/components/bar-chart/bar-chart.component.jsx
@@ -1,7 +1,47 @@
+import { useEffect } from 'react';
+import * as d3 from 'd3'
 
 const BarChart = () => {
+
+    const data = [
+        { year: 1, amount: 1230 },
+        { year: 2, amount: 2500 },
+        { year: 3, amount: 5000}
+    ];
+
+    useEffect(() => {
+        const height = 400;
+
+        const svg = d3.select('svg'); 
+        const chart = svg.append('g')
+            .attr('transform', 'translate(50, 50)');
+
+        const yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, Math.max(...data.map(d => d.amount))]);
+        chart.append('g')
+            .call(d3.axisLeft(yScale));
+
+         const xScale = d3.scaleBand()
+            .range([0, 1000])
+            .domain(data.map(d => `Year ${d.year}`))
+            .padding(0.2);
+        chart.append('g')
+            .attr('transform', `translate(0, ${height})`)
+            .call(d3.axisBottom(xScale)); 
+
+        chart.selectAll()
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', d => xScale(`Year ${d.year}`))
+            .attr('y', d => yScale(d.amount))
+            .attr('height', d => height - yScale(d.amount))
+            .attr('width', xScale.bandwidth());
+    }, []);
+
     return (
-        <b>Bar chart</b>
+        <svg id="bar-chart" width="100%" height="500px"></svg>
     );
 }
 
