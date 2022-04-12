4b4edbc1e45db43bd92d0aaa426ba664879bf7dc
Csaba Boros <boros.csaba94@gmail.com>
2022-04-12 22:30:19 +0200

    Pie chart design

--- src/components/pie-chart/pie-chart.component.jsx
+++ src/components/pie-chart/pie-chart.component.jsx
@@ -1,6 +1,7 @@
-import { zip } from "d3";
 import { useEffect } from "react"
 import * as d3 from 'd3'
+
+import './pie-chart.component.scss'
 
 const PieChart = ({ data }) => {
     
@@ -8,19 +9,19 @@ const PieChart = ({ data }) => {
         const percentages = [
             {
                 id: 'pie-chart-value-1',
-                name: 'x',
+                name: 'Total interest earned',
                 color: '#8bc34a',
                 value: 20
             },
             {
                 id: 'pie-chart-value-2',
-                name: 'y',
+                name: 'Total contributions',
                 color: '#03a9f4',
                 value: 30
             },
             {
                 id: 'pie-chart-value-3',
-                name: zip,
+                name: 'Starting amount',
                 color: '#fbc02d',
                 value: 50
             }
@@ -30,8 +31,8 @@ const PieChart = ({ data }) => {
         svg.selectAll('*').remove();
         const width = svg.attr('width');
         const height = svg.attr('height');
-        const radius = width/3;
-        const hoverRadius = width * 2/5;
+        const radius = width * 0.3;
+        const hoverRadius = width * 0.32;
 
         var g = svg.append('g')
             .attr('transform', `translate(${width / 2},${height / 2})`);
@@ -62,22 +63,39 @@ const PieChart = ({ data }) => {
             .attr('id', (d) => d.data.id)
             .attr('fill', 'transparent')
             .on('mouseover', mouseOverHandler)
-            .on('mouseout', mouseOutHandler);
+            .on('mouseout', mouseOutHandler)
+            .on('mousemove', mouseMoveHandler);
 
     }, data);
 
     return (
         <div className="pie-chart-container">
+            <div className="pie-chart-tooltip">
+                <span className="pie-chart-tooltip-label">Start amount</span><br />
+                <b className="pie-chart-tooltip-value">$12345</b>
+            </div>
             <svg id="pie-chart" width="300" height="300"></svg>
         </div>
     )
 }
 
-const mouseOverHandler = (_, item) => {
-
-    console.log(item.data);
+const mouseOverHandler = (event, item) => {
     d3.select(`#${item.data.id}`)
         .attr('fill', item.data.color);
+    const tooltip = d3.select('.pie-chart-tooltip');
+    tooltip.style('visibility', 'visible')
+        .style('left', `${event.pageX + 10}px`)
+        .style('top', `${event.pageY - 80}px`);
+    d3.select('.pie-chart-tooltip-label')
+        .html(item.data.name);
+    d3.select('.pie-chart-tooltip-value')
+        .html(`$${(+item.data.value.toFixed(0)).toLocaleString()}`);
+}
+
+const mouseMoveHandler = (event) => {
+    const tooltip = d3.select('.pie-chart-tooltip');
+    tooltip.style('left', `${event.pageX + 10}px`)
+        .style('top', `${event.pageY - 80}px`);
 }
 
 const mouseOutHandler = (_, item) => {
@@ -85,6 +103,8 @@ const mouseOutHandler = (_, item) => {
     console.log(item.data);
     d3.select(`#${item.data.id}`)
         .attr('fill', 'transparent');
+    const tooltip = d3.select('.pie-chart-tooltip');
+        tooltip.style('visibility', 'hidden');
 }
 
 export default PieChart
