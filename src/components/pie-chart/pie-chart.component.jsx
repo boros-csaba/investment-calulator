a2e8ade445e22a0b095fc105f3dfa4a82752480f
Csaba Boros <boros.csaba94@gmail.com>
2022-04-12 20:53:25 +0200

    Pie chart hover effect

--- src/components/pie-chart/pie-chart.component.jsx
+++ src/components/pie-chart/pie-chart.component.jsx
@@ -7,15 +7,21 @@ const PieChart = ({ data }) => {
     useEffect(() => {
         const percentages = [
             {
+                id: 'pie-chart-value-1',
                 name: 'x',
+                color: '#8bc34a',
                 value: 20
             },
             {
+                id: 'pie-chart-value-2',
                 name: 'y',
+                color: '#03a9f4',
                 value: 30
             },
             {
+                id: 'pie-chart-value-3',
                 name: zip,
+                color: '#fbc02d',
                 value: 50
             }
         ];
@@ -24,14 +30,15 @@ const PieChart = ({ data }) => {
         svg.selectAll('*').remove();
         const width = svg.attr('width');
         const height = svg.attr('height');
-        const radius = 200;
+        const radius = width/3;
+        const hoverRadius = width * 2/5;
 
         var g = svg.append('g')
             .attr('transform', `translate(${width / 2},${height / 2})`);
 
         const scale = d3.scaleOrdinal()
             .domain(percentages)
-            .range(['#8bc34a', '#03a9f4', '#fbc02d']);
+            .range(percentages.map(d => d.color));
 
         const pie = d3.pie().value((d) => d.value);
         
@@ -42,18 +49,42 @@ const PieChart = ({ data }) => {
         const path = d3.arc()
             .outerRadius(radius)
             .innerRadius(0);
+        const hoverPath = d3.arc()
+            .outerRadius(hoverRadius)
+            .innerRadius(0);
 
         arc.append('path')
             .attr('d', path)
             .attr('fill', (d) => scale(d.data.name));
 
+        arc.append('path')
+            .attr('d', hoverPath)
+            .attr('id', (d) => d.data.id)
+            .attr('fill', 'transparent')
+            .on('mouseover', mouseOverHandler)
+            .on('mouseout', mouseOutHandler);
+
     }, data);
 
     return (
         <div className="pie-chart-container">
-            <svg id="pie-chart" width="500" height="400"></svg>
+            <svg id="pie-chart" width="300" height="300"></svg>
         </div>
     )
+}
+
+const mouseOverHandler = (_, item) => {
+
+    console.log(item.data);
+    d3.select(`#${item.data.id}`)
+        .attr('fill', item.data.color);
+}
+
+const mouseOutHandler = (_, item) => {
+
+    console.log(item.data);
+    d3.select(`#${item.data.id}`)
+        .attr('fill', 'transparent');
 }
 
 export default PieChart
