fd8a995e4fe4395308f81cda26f37878098ae92e
Csaba Boros <boros.csaba94@gmail.com>
2022-05-10 23:52:45 +0200

    Root reducer

---
+++ src/store/inputs/inputs.reducer.js
@@ -0,0 +1,50 @@
+
+export const INPUTS_ACTION_TYPES = {
+    SET_START_AMOUNT: 'SET_START_AMOUNT',
+    SET_ADDITIONAL_CONTRIBUTION: 'SET_ADDITIONAL_CONTRIBUTION',
+    SET_RATE_OF_RETURN: 'SET_RATE_OF_RETURN',
+    SET_YEARS: 'SET_YEARS',
+    SET_FREQUENCY: 'SET_FREQUENCY'
+}
+
+const INITIAL_STATE = {
+    startAmount: 10000,
+    additionalContribution: 1000,
+    rateOfReturn: 6.0,
+    years: 20,
+    frequency: 3
+}
+
+export const inputsReducer = (state = INITIAL_STATE, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case INPUTS_ACTION_TYPES.SET_START_AMOUNT:
+            return {
+                ...state,
+                startAmount: payload
+            };
+        case INPUTS_ACTION_TYPES.SET_ADDITIONAL_CONTRIBUTION:
+            return {
+                ...state,
+                additionalContribution: payload
+            };
+        case INPUTS_ACTION_TYPES.SET_RATE_OF_RETURN:
+            return {
+                ...state,
+                rateOfReturn: payload
+            };
+        case INPUTS_ACTION_TYPES.SET_YEARS:
+            return {
+                ...state,
+                years: payload
+            };
+        case INPUTS_ACTION_TYPES.SET_FREQUENCY:
+            return {
+                ...state,
+                frequency: payload
+            };
+        default:
+            return state;
+    }
+}
---
+++ src/store/root-reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import { inputsReducer } from './inputs/inputs.reducer'
+
+export const rootReducer = combineReducers({
+    inputsReducer
+})
